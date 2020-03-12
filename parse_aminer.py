6b8f3813c91e3e6437ff12836b114658d21c0b8a
lfdversluis <lfdversluis@gmail.com>
2020-03-12 12:49:59 +0100

    Return true/false if things went well/wrong

--- parse_aminer.py
+++ parse_aminer.py
@@ -10,57 +10,61 @@ logger = logging.getLogger(__name__)
 
 
 def parse_aminer_corpus_file(path, database_path="aip.db"):
-    database = DatabaseManager(location=database_path)
-    with open(path, "r", encoding='ISO-8859-1') as json_file:
-        # print(path)
-        # The json files contain stacked json objects, which is bad practice.
-        # It should be wrapped in a JSON array.
-        # Libraries will throw errors if you attempt to load the file, so now we lazy load each object.
-        publication_iterator = iterload_file_lines(json_file)
-        for publication in publication_iterator:
-            if publication is None:  # Corrupt JSON line possibly. Skip it.
-                continue
+    try:
+        database = DatabaseManager(location=database_path)
+        with open(path, "r", encoding='ISO-8859-1') as json_file:
+            # print(path)
+            # The json files contain stacked json objects, which is bad practice.
+            # It should be wrapped in a JSON array.
+            # Libraries will throw errors if you attempt to load the file, so now we lazy load each object.
+            publication_iterator = iterload_file_lines(json_file)
+            for publication in publication_iterator:
+                if publication is None:  # Corrupt JSON line possibly. Skip it.
+                    continue
 
-            # Try to match the publication to a venue we are interested in.
-            # Warning: contrary to the documentation, the key is "venue" NOT "venue.raw"!
-            if 'venue' not in publication:
-                logger.warning("Skipping line missing venue: %s in %s.", publication, path)
-                continue
+                # Try to match the publication to a venue we are interested in.
+                # Warning: contrary to the documentation, the key is "venue" NOT "venue.raw"!
+                if 'venue' not in publication:
+                    logger.warning("Skipping line missing venue: %s in %s.", publication, path)
+                    continue
 
-            if 'title' not in publication:
-                logger.warning("Skipping line missing title: %s in %s.", publication, path)
-                continue
+                if 'title' not in publication:
+                    logger.warning("Skipping line missing title: %s in %s.", publication, path)
+                    continue
 
-            venue_string = publication['venue']
+                venue_string = publication['venue']
 
-            # Sometimes the venue string is yet another dict...
-            if isinstance(venue_string, dict) and "raw" in venue_string:
-                venue_string = venue_string["raw"]
+                # Sometimes the venue string is yet another dict...
+                if isinstance(venue_string, dict) and "raw" in venue_string:
+                    venue_string = venue_string["raw"]
 
-            publication_title = str(publication['title']).rstrip(".")
-            publication_abstract = publication['abstract'] if 'abstract' in publication else ""
+                publication_title = str(publication['title']).rstrip(".")
+                publication_abstract = publication['abstract'] if 'abstract' in publication else ""
 
-            publication_year = publication['year'] if 'year' in publication else -1
-            publication_journal_volume = publication['volume'] if 'volume' in publication else None
-            # publication_keywords = publication['keywords']
-            publication_id = publication['id']
-            citation_count = int(publication['n_citation']) if "n_citation" in publication else -1
+                publication_year = publication['year'] if 'year' in publication else -1
+                publication_journal_volume = publication['volume'] if 'volume' in publication else None
+                # publication_keywords = publication['keywords']
+                publication_id = publication['id']
+                citation_count = int(publication['n_citation']) if "n_citation" in publication else -1
 
-            publication_doi = publication['doi'] if 'doi' in publication else None
-            # Sometimes in the urls, a doi link is used. If there is, we attempt to extract the doi from the link.
-            if publication_doi is None or len(publication_doi) == 0:
-                publication_doi_urls = publication['url'] if 'url' in publication else []
-                for publication_doi_url in publication_doi_urls:
-                    if "doi.org/" in publication_doi_url:
-                        publication_doi = publication_doi_url[
-                                          publication_doi_url.index("doi.org/") + len("doi.org/"):]
-                        break
+                publication_doi = publication['doi'] if 'doi' in publication else None
+                # Sometimes in the urls, a doi link is used. If there is, we attempt to extract the doi from the link.
+                if publication_doi is None or len(publication_doi) == 0:
+                    publication_doi_urls = publication['url'] if 'url' in publication else []
+                    for publication_doi_url in publication_doi_urls:
+                        if "doi.org/" in publication_doi_url:
+                            publication_doi = publication_doi_url[
+                                              publication_doi_url.index("doi.org/") + len("doi.org/"):]
+                            break
 
-            database.update_or_insert_paper(id=publication_id, doi=publication_doi, title=publication_title,
-                                            abstract=publication_abstract, raw_venue_string=venue_string,
-                                            year=publication_year, volume=publication_journal_volume,
-                                            num_citations=citation_count)
-    # database.flush_missing_venues()
+                database.update_or_insert_paper(id=publication_id, doi=publication_doi, title=publication_title,
+                                                abstract=publication_abstract, raw_venue_string=venue_string,
+                                                year=publication_year, volume=publication_journal_volume,
+                                                num_citations=citation_count)
+        # database.flush_missing_venues()
+        return True
+    except:
+        return False
 
 
 def complement_with_aminer(aminer_root):
--- parse_semantic_scholar.py
+++ parse_semantic_scholar.py
@@ -7,50 +7,54 @@ from util import iterload_file_lines
 
 
 def parse_semantic_scholar_corpus_file(path, database_path="aip.db"):
-    database = DatabaseManager(location=database_path)
-    with open(path, "r", encoding="ISO-8859-1") as json_file:
-        # print(corpus_file)
-        # The json files contain stacked json objects, which is bad practice. It should be wrapped in a JSON array.
-        # Libraries will throw errors if you attempt to load the file, so now we lazy load each object.
-        publication_iterator = iterload_file_lines(json_file)
-        for publication in publication_iterator:
-            if publication is None:  # Corrupt JSON line possibly. Skip it.
-                continue
+    try:
+        database = DatabaseManager(location=database_path)
+        with open(path, "r", encoding="ISO-8859-1") as json_file:
+            # print(corpus_file)
+            # The json files contain stacked json objects, which is bad practice. It should be wrapped in a JSON array.
+            # Libraries will throw errors if you attempt to load the file, so now we lazy load each object.
+            publication_iterator = iterload_file_lines(json_file)
+            for publication in publication_iterator:
+                if publication is None:  # Corrupt JSON line possibly. Skip it.
+                    continue
 
-            if "venue" not in publication:  # While parsing we sometimes get KeyError: 'venue'...
-                continue
+                if "venue" not in publication:  # While parsing we sometimes get KeyError: 'venue'...
+                    continue
 
-            # Try to match the publication to a venue we are interested in.
-            # Wrap in str() as it sometimes is an int (???)
-            venue_string = str(publication['venue'])
-            if len(venue_string) == 0:
-                continue
+                # Try to match the publication to a venue we are interested in.
+                # Wrap in str() as it sometimes is an int (???)
+                venue_string = str(publication['venue'])
+                if len(venue_string) == 0:
+                    continue
 
-            # Check if any of the venue strings are a substring of the mentioned value, add it to that set.
-            publication_title = publication['title']
-            publication_abstract = publication['paperAbstract']
-            publication_year = publication['year'] if 'year' in publication else -1
-            publication_journal_volume = publication['journalVolume'].replace(" ",
-                                                                              "_")  # Empty for conferences.
-            # publication_keywords = publication['entities']
-            publication_id = publication['id']
+                # Check if any of the venue strings are a substring of the mentioned value, add it to that set.
+                publication_title = publication['title']
+                publication_abstract = publication['paperAbstract']
+                publication_year = publication['year'] if 'year' in publication else -1
+                publication_journal_volume = publication['journalVolume'].replace(" ",
+                                                                                  "_")  # Empty for conferences.
+                # publication_keywords = publication['entities']
+                publication_id = publication['id']
 
-            num_citations = 0
-            if "inCitations" in publication:
-                num_citations = len(publication["inCitations"])
+                num_citations = 0
+                if "inCitations" in publication:
+                    num_citations = len(publication["inCitations"])
 
-            publication_doi = publication['doi']
-            if publication_doi is None or len(publication_doi) == 0:
-                publication_doi_url = publication['doiUrl']
-                if "doi.org/" in publication_doi_url:
-                    publication_doi = publication['doiUrl'][
-                                      publication['doiUrl'].index("doi.org/") + len("doi.org/"):]
+                publication_doi = publication['doi']
+                if publication_doi is None or len(publication_doi) == 0:
+                    publication_doi_url = publication['doiUrl']
+                    if "doi.org/" in publication_doi_url:
+                        publication_doi = publication['doiUrl'][
+                                          publication['doiUrl'].index("doi.org/") + len("doi.org/"):]
 
-            database.update_or_insert_paper(id=publication_id, doi=publication_doi, title=publication_title,
-                                            abstract=publication_abstract, raw_venue_string=venue_string,
-                                            year=publication_year, volume=publication_journal_volume,
-                                            num_citations=num_citations)
+                database.update_or_insert_paper(id=publication_id, doi=publication_doi, title=publication_title,
+                                                abstract=publication_abstract, raw_venue_string=venue_string,
+                                                year=publication_year, volume=publication_journal_volume,
+                                                num_citations=num_citations)
         # database.flush_missing_venues()
+        return True
+    except:
+        return False
 
 
 def parse(corpus_location):
