08a05a437a561d060e486a3d0571aa7c7b17b321
lversluis <lfdversluis@gmail.com>
2020-03-27 10:45:35 +0100

    Make sure aip.db exists and added checks and doc

--- renew_data_dask.py
+++ renew_data_dask.py
@@ -9,18 +9,22 @@ from dask_jobqueue import SLURMCluster
 import parse_aminer
 import parse_dblp
 import parse_semantic_scholar
+from database_manager import DatabaseManager
 
-cluster = SLURMCluster(cores=16, memory="64 GB", processes=16, local_directory="./aip-logs", interface='ib0',
-                       walltime='02:00:00')
+number_of_cores_per_node = 16  # DAS-5 features 2x8 NUMA cores per compute node
+reservation_length = "02:00:00"  # 2 hours is more than enough... probably
+cluster = SLURMCluster(cores=number_of_cores_per_node, memory="64 GB", processes=number_of_cores_per_node,
+                       local_directory="./aip-logs", interface='ib0', walltime=reservation_length)
 
 file_locations = "/var/scratch/atlarge/aip_data"
 local_database_location = "/tmp/aip.db"
 
 data_files = []
 
+# Create a list of all the files we want to parse. Skip the compressed sources if they are still lingering around
 for path, subdirs, files in os.walk(file_locations):
     for name in files:
-        if isfile(os.path.join(path, name)):
+        if isfile(os.path.join(path, name)) and not name.endswith(("gz", "zip", "tar")):
             data_files.append(os.path.join(path, name))
 
 
@@ -34,24 +38,31 @@ def process_file(path):
 
 
 def copy_database_to_home_folder():
-    h = hash(get_worker())
+    h = hash(get_worker())  # Use the hash of the node name as database file.
     os.rename(local_database_location, "~/{}.db".format(h))
 
+
+# Grab 5 execution nodes -> 80 cores
 cluster.scale_up(5)
 client = Client(cluster)
+
+# Create one task per file.
 tasks = list(map(delayed(process_file), data_files))
 true_false_array = dd.from_delayed(tasks)
-if False not in true_false_array:
+if False not in true_false_array:  # If everything went alright, let all nodes copy their databases to the home folder.
     client.run(copy_database_to_home_folder())
 else:
     print("Parsing one of the files went horribly wrong, quitting!")
     exit(-1)
 
-import sqlite3
-con3 = sqlite3.connect("aip.db")
+# Now, each of the nodes has a local database file, we will now combine these databases into one.
+# We do this process sequentially, because we are not sure yet if SQLite likes it if all nodes do this in parallel.
+# TODO: test if we can do this procedure in each node through the copy_database_to_home_folder, would save copying data
+database_manager = DatabaseManager()  # This creates an empty aip.db if it doesn't exists.
+con3 = database_manager.db  # Reuse the connection
 
 # based on https://stackoverflow.com/a/37138506
-for worker in cluster.workers:
+for worker in cluster.workers:  # For each worker, copy all the data
     con3.execute("ATTACH '{}.db' as dba".format(hash(worker)))
 
     con3.execute("BEGIN")
@@ -61,6 +72,8 @@ for worker in cluster.workers:
         con3.execute(combine)
     con3.execute("detach database dba")
     con3.commit()
+    # Now, delete the database as it has been copied.
+    # os.remove("{}.db".format(hash(worker)))
 
 # Release the nodes
 await cluster.scale_down(cluster.workers)
